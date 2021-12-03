e8c13353e6cac98f03aec884ab14e4bc68734093
Niklas Larsson <niklas@mm.st>
2021-12-03 23:21:39 +0100

    Day 1 in C#

---
+++ csharp/day1/Program.cs
@@ -0,0 +1,10 @@
+﻿var input = File.ReadAllLines("d:/Niklas/repos/aoc2021/data/input1.txt")
+                .Where(s => s.Length > 0).Select(s => int.Parse(s));
+
+var count = input.Zip(input.Skip(1), (a, b) => a < b).Count(t => t);
+
+Console.WriteLine(count);
+
+var count2 = input.Zip(input.Skip(3), (a, b) => a < b).Count(t => t);
+
+Console.WriteLine(count2);
